498ff69d82bbf528662865fafb92b4eeb45d0b5f
Javedmv <javedmv777@gmail.com>
2024-05-24 17:56:34 +0530

    completed

--- src/App.jsx
+++ src/App.jsx
@@ -9,21 +9,25 @@ function App() {
     expectedReturn: 6,
     duration: 10
   });
+  
+  const inputIsValid = userInput.duration >= 1; 
 
   function handleChange(inputIdentifier,newValue){
     setUserInput(prevUserInput => {
         return {
             ...prevUserInput,
-            [inputIdentifier]: newValue
+            [inputIdentifier]: +newValue
         }
     });
   }
+
 
   return (
     <>
       <Header />
       <Userinput onChange={handleChange} userInput={userInput}/>
-      <Results input={userInput}/>
+      {!inputIsValid && (<p className='center'>Please enter a duration greater than zero.</p>)}
+      {inputIsValid && <Results input={userInput}/>}
     </>
   )
 }
